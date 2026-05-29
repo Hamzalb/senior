7b37a9d320772862fe6a1608c5c62b32712241ec
hamzalb <hamzaloubani1234@gmail.com>
2026-05-30 02:13:34 +0300

    j

--- senior-frontend-master/src/app/layout.tsx
+++ senior-frontend-master/src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
-      <body className="antialiased bg-surface text-white font-sans">
+      <body className="antialiased bg-surface text-white font-sans overflow-x-hidden">
         <ErrorBoundary>
           <AuthProvider>
             <NotificationProvider>
